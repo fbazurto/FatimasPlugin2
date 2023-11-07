5eee4d55e93fd586d78a6dbfe450ed0420d4ffa2
fbazurto <146509663+fbazurto@users.noreply.github.com>
2023-11-07 15:57:11 -0800

    Add files via upload

---
+++ src/main/java/my/stuff/Animal.java
@@ -0,0 +1,13 @@
+package my.stuff;
+
+public class Animal {
+
+	void bark(){//declare bark method without return type
+		System.out.println("Woof-Woof!"); //to use class and methods we need an object
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/main/java/my/stuff/MyClass.java
@@ -0,0 +1,10 @@
+package my.stuff;
+
+public class MyClass {
+	
+	public static void main(String[] args) {
+		Animal dog = new Animal(); // dog is an object of type Animal
+		dog.bark(); //object. dot. to call method. dot used to call attributes and methods
+	}
+
+}
